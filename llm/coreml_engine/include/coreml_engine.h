4f21bab5d04e54028d48ca5e9a1b0ee4d78580c5
Ishank Agrawal <ishank@mit.edu>
2024-02-02 23:21:40 -0500

    converstion between matrix3d to NSArray works

--- llm/coreml_engine/include/coreml_engine.h
+++ llm/coreml_engine/include/coreml_engine.h
@@ -1,13 +1,13 @@
 #ifndef COREML_ENGINE_H
 #define COREML_ENGINE_H
 
-#include <iostream>
-#include <fstream>
+#include <CoreML/CoreML.h>
+#include "common.h"
 
 void CoreML_init();
-
 void CoreML_exit();
-
 void CoreML_log(const char * message_format, ...);
+void CoreML_handle_errors (NSError *error);
+MLMultiArray * Matrix3D_to_MLMultiArray(Matrix3D<float> input);
 
 #endif
--- llm/coreml_engine/src/coreml_engine.mm
+++ llm/coreml_engine/src/coreml_engine.mm
@@ -1,12 +1,82 @@
 #include "coreml_engine.h"
 #include "CoreML/CoreML.h"
 
+#include <stdexcept>
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
-bool __COREML_RUNNING = false;
 
+bool __COREML_RUNNING = false;
 FILE *__COREML_LOG_FP;
+
+MLMultiArray * create_array (int dim) {
+    NSError *error = nil;
+
+    MLMultiArray *multiarray = [[MLMultiArray alloc] 
+        initWithShape:@[@(dim), @128]
+        dataType:MLMultiArrayDataTypeFloat
+        error: &error];
+
+    CoreML_handle_errors(error);
+
+    return multiarray;
+}
+
+void test_conversion() {
+    auto x = create_array(40);
+    CoreML_log("40x128 stride: [%d, %d]\n", x.strides[0].intValue, x.strides[1].intValue);
+
+    int a = 5;
+    int b = 4;
+    int c = 3;
+    float * arr = (float *) malloc(a * b * c * sizeof(float));
+    CoreML_log("arr located at %p\n", (void*) arr);
+
+    Matrix3D<float> matrix = Matrix3D<float>(arr, a, b, c);
+
+    // populate randomly
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            for (int k = 0; k < c; k++) {
+                matrix(i, j, k) = (float) (((double) rand()) / ((double) RAND_MAX));
+            }
+        }
+    }
+    
+    // print current matrix
+    CoreML_log("Inital matrix: \n");
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            CoreML_log(">  ");
+            for (int k = 0; k < c; k++) {
+                CoreML_log("%.2f ", matrix(i, j, k));
+            }
+            CoreML_log("\n");
+        }
+        CoreML_log("> \n");
+    }
+
+    // convert
+    MLMultiArray * matrix_objc = Matrix3D_to_MLMultiArray(matrix);
+
+    // print converted matrix
+    CoreML_log("Converted matrix: \n");
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            CoreML_log("> ");
+            for (int k = 0; k < c; k++) {
+                CoreML_log("%.2f ", [[matrix_objc objectForKeyedSubscript:@[@(i), @(j), @(k)]] floatValue]);
+            }
+            CoreML_log("\n");
+        }
+        CoreML_log("> \n");
+    }
+
+    // free!
+    free(arr);
+}
 
 
 void CoreML_init() {
@@ -16,18 +86,53 @@ void CoreML_init() {
     }
     __COREML_RUNNING = true;
 
+    // initialize rand
+    srand(0);
 
-    const char * __COREML_LOG_PATH = PROJECT_PATH "/llm/logs/coreml.out";
-    __COREML_LOG_FP = fopen(__COREML_LOG_PATH, "w");
+    const char * log_path = PROJECT_PATH "/llm/logs/coreml.out";
+    __COREML_LOG_FP = fopen(log_path, "w");
     if (__COREML_LOG_FP == NULL) {
-        assert(0 && "Core ML log Could not be opened!\n");
+        throw std::runtime_error("Core ML log Could not be opened!\n");
         return;
     }
+    CoreML_log("CoreML log opened at %s\n", log_path);
 
-    CoreML_log("CoreML log opened at %s\n", __COREML_LOG_PATH);
+    NSError * error;
 
-    const char * __COREML_MATMUL_128_PATH = PROJECT_PATH "/coreml/modules/matmul_transpose_128.mlpackage";
-    CoreML_log("CoreML module path: %s\n", __COREML_LOG_PATH);
+    NSURL * matmul_transpose_128_module_path = [NSURL URLWithString:@(
+        PROJECT_PATH "/coreml/modules/matmul_transpose_128.mlpackage")];
+    CoreML_log("CoreML module path: %s\n", [[matmul_transpose_128_module_path path] UTF8String]);
+
+    NSURL * matmul_transpose_128_compiled_path = [MLModel compileModelAtURL:matmul_transpose_128_module_path error:&error];
+    CoreML_handle_errors(error);
+
+    MLModelConfiguration *coreml_config = [MLModelConfiguration new];
+    coreml_config.computeUnits = MLComputeUnitsAll;
+
+    MLModel * model = [MLModel
+        modelWithContentsOfURL: matmul_transpose_128_compiled_path 
+        configuration: coreml_config
+        error: &error];
+    CoreML_handle_errors(error);
+
+    MLMultiArray * arr1 = create_array(40);
+    MLMultiArray * arr2 = create_array(40);
+
+    NSDictionary<NSString *, id> *featureDictionary = @{
+        @"A": arr1,
+        @"B": arr2,
+    };
+
+    CoreML_log("starting nn inference!\n");
+
+    MLDictionaryFeatureProvider *inFeatures = [[MLDictionaryFeatureProvider alloc] initWithDictionary:featureDictionary error:&error];
+    CoreML_handle_errors(error);
+
+    CoreML_log("finished inference!\n");
+
+    CoreML_log("testing conversion!\n");
+    test_conversion();
+    CoreML_log("finished conversion!\n");
 
     CoreML_log("CoreML Engine succesfully initiated!\n");
 };
@@ -53,4 +158,39 @@ void CoreML_log (const char * message, ...) {
     fflush(__COREML_LOG_FP);
 
     va_end(args);
+}
+
+void CoreML_handle_errors (NSError *error) {
+    if (error != nil) {
+        const char * error_str = [[NSString stringWithFormat:@"%@", [error userInfo]] UTF8String];
+        CoreML_log(error_str);
+        throw std::runtime_error(error_str);
+    }
+}
+
+MLMultiArray * Matrix3D_to_MLMultiArray(Matrix3D<float> input) {
+    CoreML_log("Attempting to create an MLMultiArray!\n");
+    NSError *error = nil;
+
+    NSArray * strides = @[@(input.m_dim_y * input.m_dim_z), @(input.m_dim_z), @1];
+    CoreML_log("Input strides %d, %d, %d\n",
+        [strides[0] intValue],
+        [strides[1] intValue],
+        [strides[2] intValue]);
+
+    MLMultiArray * multiarray = [[MLMultiArray alloc]
+        initWithDataPointer: ((void*) input.m_data)
+        shape: @[@(input.m_dim_x), @(input.m_dim_y), @(input.m_dim_z)]
+        dataType: MLMultiArrayDataTypeFloat32
+        strides: @[@(input.m_dim_y * input.m_dim_z), @(input.m_dim_z), @(1)]
+        deallocator: nil
+        error: &error
+    ];
+
+    CoreML_handle_errors(error);
+
+
+    CoreML_log("Output strides %d, %d, %d\n", multiarray.strides[0].intValue, multiarray.strides[1].intValue, multiarray.strides[2].intValue);
+
+    return multiarray;
 }
